06ad080de146527a9aede76fada43c1258412a4e
Seonghwan Hong <89226363+seonghwh@users.noreply.github.com>
2023-04-24 18:56:59 +0000

    Edited command file; removed parser

--- pkg/s23openalex/command.py
+++ pkg/s23openalex/command.py
@@ -1,22 +1,12 @@
-import argparse
+import click
 
-# from s23openalex import Works
-from .works import Works
+from s23openalex import Works
 
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--doi', type=str, help='The doi of the work.')
-    parser.add_argument('--format', type=str, choices=['ris', 'bibtex'], default='ris', help='The output format.')
-    args = parser.parse_args()
-
-    work = Works(args.doi)
-
-    if args.format == 'ris':
-        work.ris
-    elif args.format == 'bibtex':
-        work.bibtex
+@click.argument('oaid', nargs=-1)
+def main(oaid):
+    work = Works(oaid)
+    return work.ris
 
 
 if __name__ == '__main__':
-    main()
+    main(oaid)
