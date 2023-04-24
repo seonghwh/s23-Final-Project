6a13e78b499882e188383a426891df1138e5a8be
Seonghwan Hong <89226363+seonghwh@users.noreply.github.com>
2023-04-24 18:39:17 +0000

    Edited import Works in command.py file.

--- pkg/s23openalex/command.py
+++ pkg/s23openalex/command.py
@@ -1,15 +1,16 @@
 import argparse
 
-from s23openalex import Works
+# from s23openalex import Works
+from .works import Works
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--oaid', type=str, help='The OpenAlex ID of the work.')
+    parser.add_argument('--doi', type=str, help='The doi of the work.')
     parser.add_argument('--format', type=str, choices=['ris', 'bibtex'], default='ris', help='The output format.')
     args = parser.parse_args()
 
-    work = Works(args.oaid)
+    work = Works(args.doi)
 
     if args.format == 'ris':
         work.ris
