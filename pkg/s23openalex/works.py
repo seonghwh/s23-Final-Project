62b26fbeb02438b5fd856ae6cc0fb1644af14b31
Seonghwan Hong <89226363+seonghwh@users.noreply.github.com>
2023-04-24 03:28:59 +0000

    Add properties for bibtex and ris attributes.

--- pkg/s23openalex/works.py
+++ pkg/s23openalex/works.py
@@ -6,7 +6,9 @@ import base64
 import matplotlib.pyplot as plt
 from IPython.core.pylabtools import print_figure
 
+
 class Works:
+    
     def __init__(self, oaid):
         self.oaid = oaid
         self.req = requests.get(f'https://api.openalex.org/works/{oaid}')
@@ -18,6 +20,7 @@ class Works:
     def __repr__(self):
         return 'repr'
 
+    @property
     def bibtex(self):
         _authors = [au['author']['display_name'] for au in self.data['authorships']]
         if len(_authors) == 0:
@@ -44,51 +47,8 @@ class Works:
             'ENTRYTYPE': self.data['type']
         }]
         writer = BibTexWriter()
-        entry = dumps(db, writer)
-        return print(entry)
-    
-    def _repr_markdown_(self):
-        _authors = [f'[{au["author"]["display_name"]}]({au["author"]["id"]})' for au in self.data['authorships']]
-        if len(_authors) == 1:
-            authors = _authors[0]
-        else:
-            authors = ', '.join(_authors[0:-1]) + ' and ' + _authors[-1]
-            
-        title = self.data['title']
-        
-        journal = f"[{self.data['host_venue']['display_name']}]({self.data['host_venue']['id']})"
-        volume = self.data['biblio']['volume']
-        
-        issue = self.data['biblio']['issue']
-        if issue is None:
-            issue = ', '
-        else:
-            issue = ', ' + issue
-            
-        pages = '-'.join([self.data['biblio']['first_page'], self.data['biblio']['last_page']])
-        year = self.data['publication_year']
-        citedby = self.data['cited_by_count']
-        
-        oa = self.data['id']
-        
-        # Citation counts by year
-        years = [e['year'] for e in self.data['counts_by_year']]
-        counts = [e['cited_by_count'] for e in self.data['counts_by_year']]
-    
-        fig, ax = plt.subplots()
-        ax.bar(years, counts)
-        ax.set_xlabel('year')
-        ax.set_ylabel('citation count')
-        data = print_figure(fig, 'png') # save figure in string
-        plt.close(fig)
-        
-        b64 = base64.b64encode(data).decode('utf8')
-        citefig = (f'![img](data:image/png;base64,{b64})')
-        
-        s = f'{authors}, *{title}*, **{journal}**, {volume}{issue}{pages}, ({year}), {self.data["doi"]}. cited by: {citedby}. [Open Alex]({oa})'
-        
-        s += '<br>' + citefig
-        return s
+        bibtex = dumps(db, writer)
+        return print(bibtex)
     
     @property
     def ris(self):
@@ -116,7 +76,4 @@ class Works:
         fields += ['ER  -']
                 
         ris = '\n'.join(fields)
-        ris64 = base64.b64encode(ris.encode('utf-8')).decode('utf8')
-        uri = f'<pre>{ris}<pre><br><a href="data:text/plain;base64,{ris64}" download="ris">Download RIS</a>'
-        from IPython.display import HTML
-        return HTML(uri)
+        return print(ris)
