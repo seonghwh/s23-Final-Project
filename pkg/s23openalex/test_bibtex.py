7afe9abce094f98f64b220756389dd15d5c17edd
Seonghwan Hong <89226363+seonghwh@users.noreply.github.com>
2023-04-25 01:37:59 +0000

    Edited ref_bibtex in test_bibtex.py

--- pkg/s23openalex/test_bibtex.py
+++ pkg/s23openalex/test_bibtex.py
@@ -1,17 +1,7 @@
 from s23openalex import Works
 
 
-ref_bibtex = """@journal-article{https://openalex.org/W2288114809,
- author = {John R. Kitchin},
- journal = {ACS Catalysis},
- number = {6},
- pages = {3894-3899},
- title = {Examples of Effective Data Sharing in Scientific Publishing},
- url = {https://doi.org/10.1021/acscatal.5b00538},
- volume = {5},
- year = {2015}
-}
-"""
+ref_bibtex = """@journal-article{https://openalex.org/W2288114809,\n author = {John R. Kitchin},\n journal = {ACS Catalysis},\n number = {6},\n pages = {3894-3899},\n title = {Examples of Effective Data Sharing in Scientific Publishing},\n url = {https://doi.org/10.1021/acscatal.5b00538},\n volume = {5},\n year = {2015}\n}\n"""
 
 
 def test_bibtex():
