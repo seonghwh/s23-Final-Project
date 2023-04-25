4c24abf27f13e5a105f3fc1f2ca6b9903266e80d
Seonghwan Hong <89226363+seonghwh@users.noreply.github.com>
2023-04-25 02:57:46 +0000

    Edited test files to pass pylint

--- pkg/s23openalex/test_bibtex.py
+++ pkg/s23openalex/test_bibtex.py
@@ -1,7 +1,8 @@
+"""Test for bibtex attribute."""
 from s23openalex import Works
 
 
-ref_bibtex = """@journal-article{https://openalex.org/W2288114809,
+Ref_bibtex = """@journal-article{https://openalex.org/W2288114809,
  author = {John R. Kitchin},
  journal = {ACS Catalysis},
  number = {6},
@@ -15,5 +16,6 @@ ref_bibtex = """@journal-article{https://openalex.org/W2288114809,
 
 
 def test_bibtex():
-    w = Works("https://doi.org/10.1021/acscatal.5b00538")
-    assert ref_bibtex == w.bibtex
+    """Test function for bibtex."""
+    work = Works("https://doi.org/10.1021/acscatal.5b00538")
+    assert Ref_bibtex == work.bibtex
